ee1e5886c0eceaac76d67a0cdff5be46c46b8ddc
Alejandro <alejandro.mfe@gmail.com>
2022-03-10 22:23:01 -0300

    ahora carga el ng2-charts
    Solucion ->
    -      allowedCommonJsDependencies
    -     aot : false

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,13 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { RouterModule } from '@angular/router';
 import { appRoutes } from '../routes';
-
+import { ChartsModule } from "node_modules/ng2-charts/ng2-charts";
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { SectionSalesComponent } from './sections/section-sales/section-sales.component';
 import { SectionOrdersComponent } from './sections/section-orders/section-orders.component';
 import { SectionHealthComponent } from './sections/section-health/section-health.component';
+import { BarChartComponent } from './charts/bar-chart/bar-chart.component';
+import { LineChartComponent } from './charts/line-chart/line-chart.component';
+import { PieChartComponent } from './charts/pie-chart/pie-chart.component';
 
 @NgModule({
   declarations: [
@@ -18,9 +21,13 @@ import { SectionHealthComponent } from './sections/section-health/section-health
     SidebarComponent,
     SectionSalesComponent,
     SectionOrdersComponent,
-    SectionHealthComponent
+    SectionHealthComponent,
+    BarChartComponent,
+    LineChartComponent,
+    PieChartComponent
   ],
   imports: [
+    ChartsModule,
     BrowserModule,
     RouterModule.forRoot(appRoutes)
   ],
