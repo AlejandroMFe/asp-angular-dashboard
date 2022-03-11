c734823c5a375f7f9c7c2a1e1f0e1c7b0a36fb66
Alejandro <alejandro.mfe@gmail.com>
2022-03-10 22:45:56 -0300

    funcionan la bar-chart
    vamooo carajo la ctm!!!

--- client/src/app/charts/bar-chart/bar-chart.component.ts
+++ client/src/app/charts/bar-chart/bar-chart.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+const SAMPLE_BARCHART_DATA: any[] = [
+  { data: [ 65, 59, 80, 81, 56, 55, 40 ], label: 'Q3 Sales' },
+  { data: [ 28, 48, 40, 19, 86, 27, 90 ], label: 'Q4 Sales' }
+];
+
+const SAMPLE_BARCHART_LABELS: string[] = [ 'W1', 'W2', 'W3', 'W4', 'W5', 'W6', 'W7' ];
+
 @Component({
   selector: 'app-bar-chart',
   templateUrl: './bar-chart.component.html',
-  styleUrls: ['./bar-chart.component.css']
+  styleUrls: ['./bar-chart.component.css'],
 })
 export class BarChartComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  public barChartData: any[] = SAMPLE_BARCHART_DATA;
+  public barChartLabels: string[] = SAMPLE_BARCHART_LABELS;
+  public barChartType = 'bar';
+  public barChartLegend = true;
+  public barChartOptions: any = {
+    scaleShowVerticalLines: false,
+    responsive: true,
+  };
 
   ngOnInit(): void {
-  }
 
+  }
 }
