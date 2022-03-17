7e372df32d6f7f85c966f4630aec41985d272efe
Alejandro <alejandro.mfe@gmail.com>
2022-03-17 07:52:30 -0300

    created api

---
+++ advantageAPI/Models/Order.cs
@@ -0,0 +1,10 @@
+namespace advantageAPI.Models;
+
+public class Order
+{
+    public int Id { get; set; }
+    public Customer Customer { get; set; } // 1:1 relación una orden siempre pertenece a un cliente
+    public decimal Total { get; set; }
+    public DateTime Placed { get; set; }
+    public DateTime? Completed { get; set; }
+}
