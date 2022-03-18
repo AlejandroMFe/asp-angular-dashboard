e3ce5854e5b59c3642b1eec74af3553c9a2082af
Alejandro <alejandro.mfe@gmail.com>
2022-03-18 09:20:57 -0300

    add DB migration

--- advantageAPI/Program.cs
+++ advantageAPI/Program.cs
@@ -1,8 +1,16 @@
+using advantageAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration["connectionString"];
 
 // Add services to the container.
-
+builder.Services    
+       .AddDbContext<ApiContext>(
+           options => options.UseNpgsql(connectionString));
 builder.Services.AddControllers();
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
