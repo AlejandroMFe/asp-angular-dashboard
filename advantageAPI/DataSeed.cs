bcc0138713d374c6d6651c178f7f482485b68739
Alejandro <alejandro.mfe@gmail.com>
2022-03-25 08:45:09 -0300

    data seeded

--- advantageAPI/DataSeed.cs
+++ advantageAPI/DataSeed.cs
@@ -19,20 +19,109 @@ public class DataSeed
     public void SeedData(int nCustomers, int nOrders)
     {
         if (!_context.Customers.Any())
+        {
             SeedCustomers(nCustomers);
-
+            _context.SaveChanges();
+        }
         if (!_context.Orders.Any())
+        {
             SeedOrders(nOrders);
-
+            _context.SaveChanges();
+        }
         if (!_context.Servers.Any())
+        {
             SeedServers();
-
-        _context.SaveChanges();
+            _context.SaveChanges();
+        }
     }
 
     private void SeedServers()
     {
-        throw new NotImplementedException();
+        List<Server> servers = BuildServerList();
+
+        foreach (var server in servers)
+        {
+            _context.Servers.Add(server);
+        }
+    }
+
+    private List<Server> BuildServerList()
+    {
+        return new List<Server>()
+        {
+            new Server()
+            {
+                Id = 1,
+                Name = "Dev-Web",
+                IsOnline = true
+            },
+            new Server()
+            {
+                Id = 2,
+                Name = "Dev-Mail",
+                IsOnline = true
+            },
+            new Server()
+            {
+                Id = 3,
+                Name = "Dev-App",
+                IsOnline = false
+            },
+            new Server()
+            {
+                Id = 4,
+                Name = "Dev-DB",
+                IsOnline = true
+            },
+            new Server()
+            {
+                Id = 5,
+                Name = "Test-Web",
+                IsOnline = false
+            },
+            new Server()
+            {
+                Id = 6,
+                Name = "Test-Mail",
+                IsOnline = false
+            },
+            new Server()
+            {
+                Id = 7,
+                Name = "Test-App",
+                IsOnline = true
+            },
+            new Server()
+            {
+                Id = 8,
+                Name = "Test-DB",
+                IsOnline = true
+            },
+            new Server()
+            {
+                Id = 9,
+                Name = "Prod-Web",
+                IsOnline = true
+            },
+            new Server()
+            {
+                Id = 10,
+                Name = "Prod-Mail",
+                IsOnline = true
+            },
+            new Server()
+            {
+                Id = 11,
+                Name = "Prod-App",
+                IsOnline = true
+            },
+            new Server()
+            {
+                Id = 12,
+                Name = "Prod-DB",
+                IsOnline = true
+            },
+        };
     }
 
     // Siembra las ordenes en la base de datos
@@ -51,24 +140,27 @@ public class DataSeed
         var orders = new List<Order>();
         var rand = new Random();
 
-        for (int i = 0; i <= nOrders; i++)
+        for (int i = 1; i <= nOrders; i++)
         {
             // elige aleatoriamente un cliente de todos los que
             // existen en la BD
-            var randCustomerId = rand.Next(_context.Customers.Count());
-            
+            var randCustomerId = rand.Next(1, _context.Customers.Count());
+
             var placed = Helpers.GetRandomOrderPlaced();
             var completed = Helpers.GetRandomOrderCompleted(placed);
+            var customers = _context.Customers.ToList();
 
-            orders.Add(new Order
-            {
-                Id = i,
-                // relaciona la orden con uno de los clientes
-                Customer = _context.Customers.First(c => c.Id == randCustomerId),
-                Total = Helpers.GetRandomOrderTotal(),
-                Placed = placed,
-                Completed = completed 
-            });
+            orders.Add(
+                new Order
+                {
+                    Id = i,
+                    // relaciona la orden con uno de los clientes
+                    Customer = customers.First(c => c.Id == randCustomerId),
+                    Total = Helpers.GetRandomOrderTotal(),
+                    Placed = placed,
+                    Completed = completed
+                }
+            );
         }
 
         return orders;
--- advantageAPI/Program.cs
+++ advantageAPI/Program.cs
@@ -1,3 +1,4 @@
+using advantageAPI;
 using advantageAPI.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -5,11 +6,10 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration["connectionString"];
 
 // Add services to the container.
-builder.Services    
-       .AddDbContext<ApiContext>(
-           options => options.UseNpgsql(connectionString));
+builder.Services.AddDbContext<ApiContext>(options => options.UseNpgsql(connectionString));
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 builder.Services.AddControllers();
-
+builder.Services.AddTransient<DataSeed>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -17,11 +17,18 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var service = scope.ServiceProvider.GetService<DataSeed>();
+    service.SeedData(20, 1000);
 }
 
 app.UseHttpsRedirection();
